dfe8f00766a646bfd6b9354737368becaadc9570
m0rem0ney <alejandrogaya1994@gmail.com>
2019-05-24 21:29:03 +0200

    added camera functionality

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @NgModule({
   imports: [
@@ -14,4 +15,25 @@ import { Tab2Page } from './tab2.page';
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule {
+  public currentImage: any;
+
+  constructor(private camera: Camera) { }
+
+  public takePicture(): void {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options)
+      .then(imageData => {
+        this.currentImage = `data:image/jpeg;base64, ${imageData}`;
+      }, 
+      err => {
+        console.log(err);
+      });
+  }
+}
